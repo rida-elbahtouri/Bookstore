12074c8990d941068a590192e5ee11d84754cee0
Brittany Blake <brittany.blake49@gmail.com>
2020-10-06 17:36:48 +0200

    center nav and add new book form

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,27 +2,30 @@ import React from "react";
 import icon from '../assets/user.svg'
 
 const NavBar = () => (
-  <nav className="panel-bg ">
+  <nav className="">
+  <div className="panel-bg">
     <ul>
-      <li>
-        <a href="#" className="azure Bookstore-CMS">
-          Bookstore CMS
-        </a>
-      </li>
-      <li>
-        <a href="#" className="BOOKS ">
-          BOOKS
-        </a>
-      </li>
-      <li>
-        <a href="#" className=" CATEGORIES">
-          CATEGORIES
-        </a>
-      </li>
-      <li>
-        <img src={icon} className="azure Mask"></img>
-      </li>
-    </ul>
+        <li>
+          <a href="#" className="azure Bookstore-CMS">
+            Bookstore CMS
+          </a>
+        </li>
+        <li>
+          <a href="#" className="BOOKS ">
+            BOOKS
+          </a>
+        </li>
+        <li>
+          <a href="#" className=" CATEGORIES">
+            CATEGORIES
+          </a>
+        </li>
+        <li>
+          <img src={icon} className="azure Mask"></img>
+        </li>
+      </ul>
+  </div>
+    
   </nav>
 );
 
